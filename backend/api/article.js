2b196b995ffc3683dfdee48cb2803da78f8f0577
juliomchado <juliocarlos00@hotmail.com>
2020-05-09 01:34:54 +0100

    Configurando token de autenticacao

--- backend/api/article.js
+++ backend/api/article.js
@@ -1,9 +1,11 @@
+const queries = require('./queries')
+
 module.exports = app => {
     const { existsOrError } = app.api.validation
 
     const save = (req, res) => {
         const article = { ...req.body }
-        if(req.params.id) article.id = req.params.id
+        if (req.params.id) article.id = req.params.id
 
         try {
             existsOrError(article.name, 'Nome não informado')
@@ -11,11 +13,11 @@ module.exports = app => {
             existsOrError(article.categoryId, 'Categoria não informada')
             existsOrError(article.userId, 'Autor não informado')
             existsOrError(article.content, 'Conteúdo não informado')
-        } catch(msg) {
+        } catch (msg) {
             res.status(400).send(msg)
         }
 
-        if(article.id) {
+        if (article.id) {
             app.db('articles')
                 .update(article)
                 .where({ id: article.id })
@@ -28,17 +30,17 @@ module.exports = app => {
                 .catch(err => res.status(500).send(err))
         }
     }
- 
+
     const remove = async (req, res) => {
         try {
             const rowsDeleted = await app.db('articles')
                 .where({ id: req.params.id }).del()
-                try{
-            existsOrError(rowsDeleted, 'Artigo não foi encontrado.')
+            try {
+                existsOrError(rowsDeleted, 'Artigo não foi encontrado.')
 
-                }catch(msg){
-                    return res.status(400).send(msg)
-                }
+            } catch (msg) {
+                return res.status(400).send(msg)
+            }
             res.status(204).send()
         } catch (msg) {
             res.status(500).send(msg)
@@ -71,5 +73,24 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, remove, get, getById }
+    const getByCategory = async (req, res) => {
+        const categoryId = req.params.id
+
+        const page = req.query.page || 1
+        const categories = await app.db.raw(queries.CategoryWithChilden, categoryId)
+        const ids = categories.rows.map(c => c.id)
+
+        app.db({ a: 'articles', u: 'users' })
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', {author: 'u.name'})
+            .limit(limit)
+            .offset(page * limit - limit)
+            .whereRaw('?? = ??', ['u.id', 'a.userId'])
+            .whereIn('categoryId', ids)
+            .orderBy('a.id', 'desc')
+            .then(article => res.json(article))
+            .catch(err => res.status(500).send(err))
+
+    }
+
+    return { save, remove, get, getById, getByCategory }
 }
